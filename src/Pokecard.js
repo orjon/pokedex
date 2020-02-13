b6bf81657720ac7a4af7b85d844065e1a363fa49
orjon <github@orjon.com>
2020-02-13 01:41:56 +0000

    high res images implemented

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,11 +4,18 @@ import './css/Pokecard.css';
 class Pokecard extends Component {
   render() {
   const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`
+
+  let padToThree = number => (number <= 999 ? `00${number}`.slice(-3): number)
+
+  const paddedId = padToThree(this.props.id)
+
+  const imageHighRes = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`
+  
   const imageAlt = `Image of ${this.props.name}` 
     return(
       <div className='Pokecard'>
         <h3 className='Pokecard-Name'>{this.props.name}</h3>
-        <img src={image} alt={imageAlt}/>
+        <img src={imageHighRes} alt={imageAlt}/>
         <p>Type: {this.props.type}</p>
         <p>Experience: {this.props.exp}</p>
       </div>
